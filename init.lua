c534fc6ad1bfe82a8640bd2acb58f76364338597
Kennedy Mwavu <mwavukennedy@gmail.com>
2025-06-17 18:15:46 +0300

    fix: use air for formatting, disable rlanguageserver

--- init.lua
+++ init.lua
@@ -2,22 +2,22 @@
 require("config.lazy")
 
 require("lspconfig").air.setup({
-  -- on_attach = function(_, bufnr)
-  --   vim.api.nvim_create_autocmd("BufWritePre", {
-  --     buffer = bufnr,
-  --     callback = function()
-  --       vim.lsp.buf.format()
-  --     end,
-  --   })
-  -- end,
+  on_attach = function(_, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format()
+      end,
+    })
+  end,
 })
 
--- require("lspconfig").r_language_server.setup({
---   on_attach = function(client, _)
---     client.server_capabilities.documentFormattingProvider = false
---     client.server_capabilities.documentRangeFormattingProvider = false
---   end,
--- })
+require("lspconfig").r_language_server.setup({
+  on_attach = function(client, _)
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentRangeFormattingProvider = false
+  end,
+})
 
 require("gitsigns").setup({
   current_line_blame = true,

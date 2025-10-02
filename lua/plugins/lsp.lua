93d00cfc4e7ef77763f7afc7fb2dac24a22c4f1f
Kennedy Mwavu <mwavukennedy@gmail.com>
2025-10-02 17:12:18 +0300

    ft: install treesitter for svelte to ensure syntax highlighting

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,4 +7,12 @@ return {
       },
     },
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "svelte",
+      })
+    end,
+  },
 }

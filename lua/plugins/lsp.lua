deff2e1af33fad29209adb68a7d5f5e5cc4c023d
Kennedy Mwavu <mwavukennedy@gmail.com>
2025-10-02 20:32:06 +0300

    ch: highlight css in svelte

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,6 +12,9 @@ return {
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "svelte",
+        "css",
+        "javascript",
+        "typescript",
       })
     end,
   },
